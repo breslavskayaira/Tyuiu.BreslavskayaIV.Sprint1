8c4fede013052b42e0a9d1a7d05930be18a5be57
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-18 21:01:40 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint1.Task0.Lib/Tyuiu.BreslavskayaIV.Sprint1.Task0.Lib.cpp
@@ -0,0 +1,18 @@
+﻿// Tyuiu.BreslavskayaIV.Sprint1.Task0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "math.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service0 : public ISprint1Task0V00
+{
+	virtual int Calculate(int a, int b) override
+	{
+		int s;
+		s = a * b;
+		return s;
+	}
+};
---
+++ Tyuiu.BreslavskayaIV.Sprint1.Task0.Test/Tyuiu.BreslavskayaIV.Sprint1.Task0.Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.BreslavskayaIV.Sprint1.Task0.Lib/Tyuiu.BreslavskayaIV.Sprint1.Task0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint1Task0Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint1Task0Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task0V00* date = new Service0();
+			int a = 1;
+			int b = 1;
+			int s;
+			s = date->Calculate(a, b);
+			Assert::AreEqual(1, s);
+		}
+	};
+}
