2f479929dc29aebd1b64a0ebd2344b99841aa81a
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-20 22:47:37 +0500

    Add files via upload

---
+++ BreslavskayaIV.Sprint1.Task2.V4.Lib/BreslavskayaIV.Sprint1.Task2.V4.Lib.cpp
@@ -0,0 +1,25 @@
+﻿// BreslavskayaIV.Sprint1.Task2.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "math.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service0 : public ISprint1Task1
+{
+	virtual int Logic(int a) override
+	{
+		int s;
+		s = (a%10) % 3;
+		if (s == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+};
---
+++ BreslavskayaIV.Sprint1.Task2.V4.Test/BreslavskayaIV.Sprint1.Task2.V4.Test.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../BreslavskayaIV.Sprint1.Task2.V4.Lib/BreslavskayaIV.Sprint1.Task2.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint1Task0Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint1Task0Test)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task1* date = new Service0();
+			int a = 153;
+			int s;
+			s = date->Logic(a);
+			Assert::AreEqual(1, s);
+		}
+	};
+}
